3f203ad797803a8432dda5d678f35db0bc5a2d34
letulip <letulip@letulips-MacBook-Pro.local>
2018-11-14 18:10:57 +0300

    Добавь 6-е задание

--- js/main.js
+++ js/main.js
@@ -3,7 +3,31 @@
 (function () {
   const TEMPLATES = document.querySelectorAll(`template`);
   const MAIN = document.querySelector(`#main`);
+  const BODY = document.querySelector(`body`);
   let currentPageNumber = 0;
+
+  const addArrows = () => {
+    const arrows = `
+      <style>
+        .arrows__wrap {
+          position: absolute;
+          top: 95px;
+          left: 50%;
+          margin-left: -56px;
+        }
+        .arrows__btn {
+          background: none;
+          border: 2px solid black;
+          padding: 5px 20px;
+        }
+      </style>
+      <button class="arrows__btn"><-</button>
+      <button class="arrows__btn">-></button>`;
+    const arrowsBlock = document.createElement(`div`);
+    arrowsBlock.classList.add(`arrows__wrap`);
+    arrowsBlock.innerHTML += arrows;
+    BODY.append(arrowsBlock);
+  };
 
   const pageToDisplay = (page) => {
     if (page < 0) {
@@ -15,14 +39,15 @@
       currentPageNumber = TEMPLATES.length - 1;
     }
     MAIN.innerHTML = TEMPLATES[page].innerHTML;
+    addArrows();
   };
 
   const previousPageHandler = () => {
-    pageToDisplay(currentPageNumber += 1);
+    pageToDisplay(currentPageNumber -= 1);
   };
 
   const nextPageHandler = () => {
-    pageToDisplay(currentPageNumber -= 1);
+    pageToDisplay(currentPageNumber += 1);
   };
 
   document.addEventListener(`keydown`, (evt) => {
