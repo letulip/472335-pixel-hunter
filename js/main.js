9ac652dcb4523c6701fc79fc3826ceaa8152a0d0
letulip <letulip@letulips-MacBook-Pro.local>
2018-11-15 10:01:43 +0300

    Исправь замечания

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 'use strict';
 
-(function () {
-  const TEMPLATES = document.querySelectorAll(`template`);
-  const MAIN = document.querySelector(`#main`);
-  const BODY = document.querySelector(`body`);
+(() => {
+  const templates = document.querySelectorAll(`template`);
+  const main = document.querySelector(`#main`);
+  const body = document.querySelector(`body`);
   let currentPageNumber = 0;
 
   const addArrows = () => {
@@ -23,11 +23,11 @@
       </style>
       <button class="arrows__btn"><-</button>
       <button class="arrows__btn">-></button>`;
-    const arrowsBlock = document.createElement(`div`);
-    arrowsBlock.classList.add(`arrows__wrap`);
-    arrowsBlock.innerHTML += arrows;
+    const navigationElements = document.createElement(`div`);
+    navigationElements.classList.add(`arrows__wrap`);
+    navigationElements.innerHTML = arrows;
 
-    BODY.append(arrowsBlock);
+    body.append(navigationElements);
 
     const navArrows = document.querySelectorAll(`.arrows__btn`);
     navArrows[0].addEventListener(`click`, () => {
@@ -42,30 +42,34 @@
     if (page < 0) {
       page = 0;
       currentPageNumber = 0;
+      return;
     }
-    if (page >= TEMPLATES.length - 1) {
-      page = TEMPLATES.length - 1;
-      currentPageNumber = TEMPLATES.length - 1;
+    if (page >= templates.length - 1) {
+      page = templates.length - 1;
+      currentPageNumber = templates.length - 1;
+      return;
     }
-    MAIN.innerHTML = TEMPLATES[page].innerHTML;
+    main.innerHTML = templates[page].innerHTML;
   };
 
   const previousPageHandler = () => {
-    pageToDisplay(currentPageNumber -= 1);
+    pageToDisplay(--currentPageNumber);
   };
 
   const nextPageHandler = () => {
-    pageToDisplay(currentPageNumber += 1);
+    pageToDisplay(++currentPageNumber);
   };
 
   document.addEventListener(`keydown`, (evt) => {
     if (evt.code === `ArrowLeft`) {
       evt.preventDefault();
       previousPageHandler();
+      return;
     }
     if (evt.code === `ArrowRight`) {
       evt.preventDefault();
       nextPageHandler();
+      return;
     }
   });
 
