acad75a77d531cfcfb865d79f7c3d7324dc9a49b
letulip <letulip@letulips-MacBook-Pro.local>
2018-11-20 13:44:54 +0300

    Исправь шаблоны для рендера страниц на строки

--- js/intro.js
+++ js/intro.js
@@ -1,10 +1,14 @@
 import pageRender from './renderModule.js';
 import renderGreeting from './greeting.js';
 
-const intro = document.querySelector(`#intro`);
+const intro = `
+<section class="intro">
+  <button class="intro__asterisk asterisk" type="button"><span class="visually-hidden">Продолжить</span>*</button>
+  <p class="intro__motto"><sup>*</sup> Это не фото. Это рисунок маслом нидерландского художника-фотореалиста Tjalf Sparnaay.</p>
+</section>`;
 
 const renderIntro = () => {
-  pageRender(intro.innerHTML);
+  pageRender(intro);
 
   const asterisk = document.querySelector(`.asterisk`);
   asterisk.addEventListener(`click`, () => {
