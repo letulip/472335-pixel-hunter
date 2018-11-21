2eb629acb24479bde51088e9539ae4f042f82eef
letulip <letulip@letulips-MacBook-Pro.local>
2018-11-21 12:33:28 +0300

    Подключи кофе и чай

--- gulpfile.js
+++ gulpfile.js
@@ -12,6 +12,21 @@ const imagemin = require(`gulp-imagemin`);
 const svgstore = require(`gulp-svgstore`);
 const rollup = require(`gulp-better-rollup`);
 const sourcemaps = require(`gulp-sourcemaps`);
+const mocha = require(`gulp-mocha`);
+const commonjs = require(`rollup-plugin-commonjs`);
+
+gulp.task(`test`, function () {
+  return gulp
+  .src([`js/**/*.test.js`])
+  .pipe(rollup({
+    plugins: [
+      commonjs()
+    ]}, `cjs`))
+  .pipe(gulp.dest(`build/test`))
+  .pipe(mocha({
+    reporter: `spec`
+  }));
+});
 
 gulp.task(`style`, () => {
   return gulp.src(`sass/style.scss`).
