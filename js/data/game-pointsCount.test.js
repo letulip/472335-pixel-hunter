7714fcacf16a1c94a75de8bcf72a76cf1dbb4fac
letulip <letulip@letulips-MacBook-Pro.local>
2018-11-22 20:01:41 +0300

    Удали дикие тесты

--- js/data/game-pointsCount.test.js
+++ js/data/game-pointsCount.test.js
@@ -1,54 +1,10 @@
 import {assert} from 'chai';
 
-const a1 = {
-  count: 0,
-};
-
-const a2 = {
-  count: -1,
-};
-
-const a3 = {
-  count: 10,
-};
-
-const a4 = {
-  count: 0,
-};
-
-// const a5 = {
-//   count: 10,
-//   points: 1150,
-//   lives: 3,
-//   time: 50
-// };
-
-const countList = [-1, 0, 5, 10, 11];
-const pointsList = [500, 650, 1000, 1150, 1500, 1650];
-const livesList = [0, 1, 2, 3];
-const timesList = [49, 50, 51, 99, 100, 101, 150];
-
-const getRandomInt = (min, max) => {
-  return Math.floor(Math.random() * (max - min)) + min;
-};
-
-const answersObjectGenerator = () => {
-  return {
-    count: getRandomInt(0, countList.length),
-    points: getRandomInt(0, pointsList.length),
-    lives: getRandomInt(0, livesList.length),
-    time: getRandomInt(0, timesList.length)
-  };
-};
-
-const pointsCount = (answersObject) => {
-  if (!answersObject || typeof answersObject !== `object` || !answersObject.isArray) {
-    return `answersObject should be a non empty object`;
-  }
-  if (answersObject.count < 10) {
+const pointsCount = (answers) => {
+  if (answers < 10) {
     return -1;
   }
-  if (answersObject.count === 10) {
+  if (answers === 10) {
     return 1;
   }
   return false;
@@ -56,13 +12,13 @@ const pointsCount = (answersObject) => {
 
 describe(`Points Counter`, () => {
   it(`should return -1 when got less then 10 answers`, () => {
-    assert(pointsCount(a1));
-    assert(pointsCount(a2));
-    assert(pointsCount(a4));
+    assert(pointsCount(-1));
+    assert(pointsCount(0));
+    assert(pointsCount(9));
   });
 
   it(`should return 1 when got 10 answers`, () => {
-    assert(pointsCount(a3));
+    assert(pointsCount(10));
   });
 
   it(`should deal with corner cases correctly`, () => {
@@ -77,11 +33,5 @@ describe(`Points Counter`, () => {
     assert(pointsCount(true));
     assert(pointsCount({}));
     assert(pointsCount([]));
-  });
-
-  it(`should deal with valid data`, () => {
-    for (let i = 0; i < 1000; i++) {
-      assert(pointsCount(answersObjectGenerator()));
-    }
   });
 });
