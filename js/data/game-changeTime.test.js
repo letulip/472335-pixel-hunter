e13fea6afd0a4fd2733761c4e6821f16f40147ed
letulip <letulip@Mac-mini.local>
2018-11-27 15:01:24 +0300

    Измени тесты для timeCount

--- js/data/game-changeTime.test.js
+++ js/data/game-changeTime.test.js
@@ -21,14 +21,7 @@ const timerTick = () => {
   }, 1000);
 };
 
-const timeCount = (state, time) => {
-  if (typeof time !== `number`) {
-    throw new Error(`Time should be of type number`);
-  }
-  if (time < 0) {
-    throw new Error(`Time should not be negative`);
-  }
-
+const timeCount = (state) => {
   const newState = Object.assign({}, state, {
     'time': resetTimer()
   });
@@ -41,29 +34,11 @@ const resetTimer = () => {
   return defaultTimerValue;
 };
 
-describe(`Check time change`, () => {
-  // it(`should change time of game`, () => {
-  //   assert.equal(timeCount(INITIAL_STATE, 1).time, 1);
-  //   assert.equal(timeCount(INITIAL_STATE, 2).time, 2);
-  // });
-  it(`should not allow set negative values`, () => {
-    assert.throws(() => timeCount(INITIAL_STATE, -1).time, 0);
-  });
-  it(`should take only number`, () => {
-    assert.throws(() => timeCount(INITIAL_STATE, []).time, /Time should be of type number/);
-    assert.throws(() => timeCount(INITIAL_STATE, {}).time, /Time should be of type number/);
-    assert.throws(() => timeCount(INITIAL_STATE, null).time, /Time should be of type number/);
-    assert.throws(() => timeCount(INITIAL_STATE, undefined).time, /Time should be of type number/);
-    assert.throws(() => timeCount(INITIAL_STATE, ``).time, /Time should be of type number/);
+describe(`Check time count`, () => {
+  it(`should return object with default time value`, () => {
+    assert.equal(timeCount(INITIAL_STATE).time, 30);
   });
 });
-
-// describe(`Check time increase`, () => {
-//   it(`should return increased time value`, () => {
-//     assert.equal(timeCount(INITIAL_STATE, 50).time, 50);
-//     assert.equal(timeCount(timeCount(INITIAL_STATE, 150), 50).time, 200);
-//   });
-// });
 
 describe(`Check reset Timer`, () => {
   it(`should reset time value`, () => {
