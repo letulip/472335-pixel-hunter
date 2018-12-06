b396cf79dc1c509c448119ce63ccf946218ad41b
letulip <letulip@Mac-mini.local>
2018-12-06 15:51:00 +0300

    Удали старый код из game-1-2-3

--- js/game-1.js
+++ js/game-1.js
@@ -1,46 +1,4 @@
-import {gameRender, createLayoutElement} from './renderModule.js';
 import AbstractView from './AbstractView.js';
-
-const renderGame1 = (question, cb) => {
-  const game1 = `
-    <p class="game__task">${question.title}</p>
-    <form class="game__content">
-    </form>
-    <ul class="stats">
-    </ul>`;
-
-  const gameLayoutElement = createLayoutElement(`section`, game1, [`game`]);
-  const gameContent = gameLayoutElement.querySelector(`.game__content`);
-
-  question.options.forEach((option, index) => {
-    const gameOptionLayout = `
-      <img src="${option.src}" alt="Option ${index}" width="468" height="458">
-      <label class="game__answer game__answer--photo">
-        <input class="visually-hidden" name="question${index}" type="radio" value="photo">
-        <span>Фото</span>
-      </label>
-      <label class="game__answer game__answer--paint">
-        <input class="visually-hidden" name="question${index}" type="radio" value="paint">
-        <span>Рисунок</span>
-      </label>`;
-    const gameOption = createLayoutElement(`div`, gameOptionLayout, [`game__option`]);
-
-    // event listener:
-    gameOption.addEventListener(`click`, () => {
-      const inputList = gameContent.querySelectorAll(`input:checked`);
-      if (inputList.length === 2) {
-        const isCorrect = Array.prototype.every.call(inputList, (inputElement, inputIndex) => {
-          return inputElement.value === question.options[inputIndex].type;
-        });
-        cb(isCorrect);
-      }
-    });
-
-    gameContent.append(gameOption);
-  });
-
-  gameRender(gameLayoutElement);
-};
 
 class ViewGame1 extends AbstractView {
   constructor(question, cb) {
--- js/game-2.js
+++ js/game-2.js
@@ -1,44 +1,4 @@
-import {gameRender, createLayoutElement} from './renderModule.js';
 import AbstractView from './AbstractView.js';
-
-const renderGame2 = (question, cb) => {
-  const game2 = `
-    <p class="game__task">${question.title}</p>
-    <form class="game__content  game__content--wide">
-    </form>
-    <ul class="stats">
-    </ul>`;
-
-  const gameLayoutElement = createLayoutElement(`section`, game2, [`game`]);
-  const gameContent = gameLayoutElement.querySelector(`.game__content`);
-
-  question.options.forEach((option, index) => {
-    const gameOptionLayout = `
-      <img src="${option.src}" alt="Option ${index}" width="705" height="455">
-      <label class="game__answer  game__answer--photo">
-        <input class="visually-hidden" name="question${index}" type="radio" value="photo">
-        <span>Фото</span>
-      </label>
-      <label class="game__answer  game__answer--paint">
-        <input class="visually-hidden" name="question${index}" type="radio" value="paint">
-        <span>Рисунок</span>
-      </label>`;
-    const gameOption = createLayoutElement(`div`, gameOptionLayout, [`game__option`]);
-
-    // event listener:
-    const inputsList = gameOption.querySelectorAll(`input`);
-    inputsList.forEach((input) => {
-      input.addEventListener(`change`, () => {
-        cb(input.value === option.type);
-      });
-    });
-
-    gameContent.append(gameOption);
-  });
-
-
-  gameRender(gameLayoutElement);
-};
 
 class ViewGame2 extends AbstractView {
   constructor(question, cb) {
