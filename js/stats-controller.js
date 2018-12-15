571b1a0686eeef0b082be45955f9ce76be1294fc
letulip <letulip@letulips-MacBook-Pro.local>
2018-12-15 20:14:13 +0300

    Замени state на model stats-controller.js

--- js/stats-controller.js
+++ js/stats-controller.js
@@ -2,8 +2,8 @@ import ViewStats from './stats-view.js';
 import {contentRender} from './render-module.js';
 
 class StatsController {
-  static showStats(state) {
-    const stats = new ViewStats(state);
+  static showStats(model) {
+    const stats = new ViewStats(model);
     contentRender(stats.element);
   }
 }
